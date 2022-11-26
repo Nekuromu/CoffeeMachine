17c7ce3e670556e057b0bca867f0a608b99e6bea
Nekuromu <30809120+Nekuromu@users.noreply.github.com>
2022-11-25 22:08:32 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+# TODO Prompt user by asking what they would like
+# "What would you like? (espresso/latte/cappuccino): "
+
+# TODO Turn off the Coffee Machine by entering "off" to the prompt
+
+# TODO Print report. (resource values) Example:
+# Water: 100ml
+# Milk: 50 ml
+# Coffee: 76g
+# Money: $2.50
+
+# TODO Check resources sufficient
+# "Sorry there is not enough water."
+
+# TODO Process Coins (Pennies, Nickles, Dimes, Quarters)
+
+# TODO Check transaction was successful
+# "Sorry that's not enough money. Money refunded."
+
+# TODO Offer Change if too much money
+
+# TODO Make Coffee
+# "Here is your latte. Enjoy!"
